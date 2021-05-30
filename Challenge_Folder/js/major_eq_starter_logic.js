9211f0be7d2eb367eed8f8369c525f82af36a765
Simon Adeoye <mymac@Simons-MacBook-Pro.local>
2021-05-30 14:21:49 -0400

    include

--- Challenge_Folder/js/major_eq_starter_logic.js
+++ Challenge_Folder/js/major_eq_starter_logic.js
@@ -109,24 +109,60 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   allEarthquakes.addTo(map);
 
 // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
-d3.json().then(function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
 // 4. Use the same style as the earthquake data.
-
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
 
 // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
-
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
 
 // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
-
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
 
 // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
 // sets the style of the circle, and displays the magnitude and location of the earthquake
 //  after the marker has been created and styled.
 L.geoJson(, {
-    
+  pointToLayer: function(feature, latlng) {
+    console.log(data);
+    return L.circleMarker(latlng);
+  },
 });
 // 8. Add the major earthquakes layer to the map.
+allEarthquakes.addTo(map);
 
 // 9. Close the braces and parentheses for the major earthquake data.
 });
@@ -166,7 +202,10 @@ legend.onAdd = function() {
 
 
   // Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
-    
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(tdata) {
+    L.geoJson(tdata,{
+      color: "#eecc00"
+    }).addTo (tectonicplates)
+    tectonicplates.addTo(map);
+   });
   });
-});
